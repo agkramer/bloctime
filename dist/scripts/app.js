f186bd5f0b04886ad31c7fa2bd6546e310776a85
Allen Kramer <kramer.alleng@gmail.com>
2016-10-11 09:49:15 -0400

    added ui-routing

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,23 @@
-angular.module('blocTime', []);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase:false
+            });
+
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'templates/about.html'
+            });
+    }
+
+    angular
+        .module('blocTime', ['ui.router'])
+        .config(config);
+})();
