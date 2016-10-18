43fc853bcea85e3cc41f71663fd1cbd57f8e0bd8
Allen Kramer <kramer.alleng@gmail.com>
2016-10-18 14:49:05 -0400

    moved firebase initialize script into separate script, edited work_session.html from med break to sm break

---
+++ dist/scripts/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyA9wOLlrfJ13rHcqyV3QrLNIsERJNXmZyM",
+    authDomain: "bloctime-b2d52.firebaseapp.com",
+    databaseURL: "https://bloctime-b2d52.firebaseio.com",
+    storageBucket: "bloctime-b2d52.appspot.com",
+    messagingSenderId: "971553801299"
+};
+
+firebase.initializeApp(config);
